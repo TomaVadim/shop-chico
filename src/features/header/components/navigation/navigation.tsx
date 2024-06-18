dba3a620fee46f3c8c034746951154f7e76ce254
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-06-18 16:18:40 +0300

    feat: create navigation component

---
+++ src/features/header/components/navigation/navigation.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+
+import { Box, Button } from "@mui/material";
+
+import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
+import { CartButton } from "@/features/header/components/cart-button/cart-button";
+
+export const Navigation = (): JSX.Element => {
+  return (
+    <Box component="nav" className="flex items-center md:gap-5">
+      <Button
+        component={Link}
+        href={PUBLIC_ROUTES.PRODUCTS}
+        rel="noreferrer nofollow"
+        className="text-white"
+      >
+        Каталог товарів
+      </Button>
+
+      <CartButton />
+    </Box>
+  );
+};
