d0086d6731551db504838d75191ede0a8c0ad3f1
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-06-18 16:19:33 +0300

    feat: create header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,24 @@
+import { Box, Container, Paper } from "@mui/material";
+
+import { CompanyLogo } from "@/features/components/company-logo/company-logo";
+import { Navigation } from "@/features/header/components/navigation/navigation";
+import { HEADER_HEIGHT } from "@/constants/header-height";
+
+export const Header = (): JSX.Element => {
+  return (
+    <Box
+      sx={{ bgcolor: "primary.main" }}
+      component="header"
+      className={`w-full fixed top-0 left-0 z-10 shadow-md h-[${HEADER_HEIGHT}]`}
+    >
+      <Container
+        maxWidth="lg"
+        className="py-4 h-full flex justify-between items-center"
+      >
+        <CompanyLogo />
+
+        <Navigation />
+      </Container>
+    </Box>
+  );
+};
