70a0ca7627ca5744b033c7f4833e48d5d33e944e
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-06-30 23:30:03 +0300

    feat: create route to register new user

---
+++ src/app/api/auth/signup/route.ts
@@ -0,0 +1,52 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { hashPassword } from "@/utils/secure-password";
+import { connectToDB } from "@/utils/connect-to-db";
+import User from "@/schemas/user-schema";
+
+const handler = async (req: NextRequest) => {
+  try {
+    await connectToDB();
+
+    const body = await req.json();
+
+    const { email, password } = body;
+
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const user = new User({
+      email,
+      password: hashedPassword,
+    });
+
+    await user.save();
+
+    if (!user) {
+      return NextResponse.json(
+        { error: "Failed to create user" },
+        { status: 500 }
+      );
+    }
+
+    return NextResponse.json({
+      message: "Success",
+    });
+  } catch (error) {
+    console.error("Error parsing request body:", error);
+    return NextResponse.json(
+      { error: "Invalid request body" },
+      { status: 400 }
+    );
+  }
+};
+
+export { handler as POST };
