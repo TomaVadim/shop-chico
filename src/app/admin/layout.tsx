2fb40f31d370caaf71e7d2923a06533e4bf24254
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-01 01:11:02 +0300

    chore: update admin layout

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,15 @@
+import { Navigation } from "@/components/admin/navigation/navigation";
+import { NAVIGATION_LIST } from "@/features/admin/constants/navigation-list";
+
 export default function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      <Navigation list={NAVIGATION_LIST} />
+      {children}
+    </>
+  );
 }
