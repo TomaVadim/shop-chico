4a959f409934cd89fcd1286d1e640280665819d5
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-01 01:08:32 +0300

    feat: create admin panel navigation

---
+++ src/features/admin/constants/navigation-list.ts
@@ -0,0 +1,9 @@
+import { NavigationItem } from "@/features/admin/shared/interfaces/navigation-item";
+
+export const NAVIGATION_LIST: NavigationItem[] = [
+  { id: 1, href: "/admin", page: "+ Додати" },
+  { id: 2, href: "/admin", page: "Замовлення" },
+  { id: 3, href: "/admin", page: "пусто" },
+  { id: 4, href: "/admin", page: "пусто" },
+  { id: 5, href: "/admin", page: "пусто" },
+];
