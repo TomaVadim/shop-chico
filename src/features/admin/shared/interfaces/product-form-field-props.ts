b0523f0eb05a8ad195ed97e095a4101db8a89940
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-04 14:05:36 +0300

    feat: create interface product-form-field-props

--- src/features/admin/shared/interfaces/product-form-field-props.ts
+++ src/features/admin/shared/interfaces/product-form-field-props.ts
@@ -1,9 +1,8 @@
-import { Control, FieldErrors, UseFormRegister } from "react-hook-form";
+import { FieldErrors, UseFormRegister } from "react-hook-form";
 
 import { ProductFormData } from "@/features/admin/shared/types/product-form-data";
 
 export interface ProductFormFieldProps {
-  control?: Control<ProductFormData>;
   name: keyof ProductFormData;
   register: UseFormRegister<ProductFormData>;
   errors: FieldErrors<ProductFormData>;
