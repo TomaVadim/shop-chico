bb0fee79628cf79f84128c93ec4cccdf84bedabc
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-04 14:06:05 +0300

    feat: create hook to validate product-form-data

--- src/features/admin/hooks/use-validate-product-form-data.ts
+++ src/features/admin/hooks/use-validate-product-form-data.ts
@@ -2,13 +2,15 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 
 import { productFormSchema } from "@/features/admin/schemas/product-form-schema";
-import { ProductFormData } from "../shared/types/product-form-data";
+import { ProductFormData } from "@/features/admin/shared/types/product-form-data";
 
 export const useValidateProductFormData = () =>
   useForm<ProductFormData>({
     resolver: zodResolver(productFormSchema),
     defaultValues: {
-      image: null,
+      insert: "without",
+      gender: "unisex",
+      imageUrl: "",
       description: "",
       price: "",
       quantity: "",
