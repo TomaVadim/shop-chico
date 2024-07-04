16dd0a1bce3426f2d6dac6f83d7cc4b8a8d3162d
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-04 18:00:49 +0300

    feat: update product list with external data

--- src/components/products/product-card/product-card.tsx
+++ src/components/products/product-card/product-card.tsx
@@ -1,13 +1,65 @@
-import { Button, Grid, Paper } from "@mui/material";
 import Link from "next/link";
-import image from "@/assets/images/home/our-new-products/image_1.jpeg";
 import Image from "next/image";
 
-export const ProductCard = (): JSX.Element => {
+import { Button, Grid, Paper, Typography } from "@mui/material";
+
+import { translateInsert } from "@/features/products/utils/translate-insert";
+import { translateGender } from "@/features/products/utils/translate-gender";
+
+interface Props {
+  description: string;
+  gender: string;
+  imageUrl: string;
+  insert: string;
+  price: number;
+  quantity: number;
+}
+
+export const ProductCard = ({
+  gender,
+  imageUrl,
+  insert,
+  price,
+  quantity,
+}: Props): JSX.Element => {
+  const translatedGender = translateGender(gender);
+
+  const translatedInsert = translateInsert(insert);
+
   return (
     <Grid item xs={12} sm={6} md={3}>
       <Paper component="article" className="flex flex-col h-full" elevation={3}>
-        <Image src={image} alt="image" className="w-full flex-[70%]" />
+        <Image
+          priority={false}
+          src={imageUrl}
+          width={250}
+          height={270}
+          alt="Фото чохла"
+          className="w-full mt-6 flex-[70%]"
+        />
+
+        <div className="px-5 mt-6 flex justify-between items-center">
+          <Typography className="text-sm">
+            Ціна: <span className="pl-1 font-bold">{price}</span>
+          </Typography>
+
+          <Typography className="text-sm">
+            Кількість:
+            <span
+              className={`font-bold pl-1 ${quantity <= 0 && "text-red-500"}`}
+            >
+              {quantity}
+            </span>
+          </Typography>
+        </div>
+
+        <Typography className="text-sm px-5 mt-3">
+          Стать: <span className="pl-1 font-bold">{translatedGender}</span>
+        </Typography>
+
+        <Typography className="text-sm px-5 mt-3">
+          Вставка: <span className="pl-1 font-bold">{translatedInsert}</span>
+        </Typography>
 
         <div className="px-5 py-6 flex justify-between items-center">
           <Button variant="outlined" color="secondary" className="text-sm">
