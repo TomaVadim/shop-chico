06892d5677212ea0ad3d88e5e3b84b5ee0ce00f1
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-08 00:57:50 +0300

    chore: change default values

--- src/features/admin/hooks/use-validate-product-form-data.ts
+++ src/features/admin/hooks/use-validate-product-form-data.ts
@@ -3,16 +3,18 @@ import { useForm } from "react-hook-form";
 
 import { productFormSchema } from "@/features/admin/schemas/product-form-schema";
 import { ProductFormData } from "@/features/admin/shared/types/product-form-data";
+import { INSERT } from "@/shared/enums/filter/insert.filter";
+import { GENDER } from "@/shared/enums/filter/gender.filter";
 
-export const useValidateProductFormData = () =>
+export const useValidateProductFormData = (defaultData?: ProductFormData) =>
   useForm<ProductFormData>({
     resolver: zodResolver(productFormSchema),
     defaultValues: {
-      insert: "without",
-      gender: "unisex",
-      imageUrl: "",
-      description: "",
-      price: "",
-      quantity: "",
+      insert: defaultData?.insert || INSERT.WITHOUT,
+      gender: defaultData?.gender || GENDER.UNISEX,
+      imageUrl: defaultData?.imageUrl || "",
+      description: defaultData?.description || "",
+      price: defaultData?.price || "",
+      quantity: defaultData?.quantity || "",
     },
   });
