899d271d17c23441a6a1b5184b16d8e693981ec4
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-08 20:45:07 +0300

    feat: create cart-layout

---
+++ src/app/cart/layout.tsx
@@ -0,0 +1,31 @@
+import { Typography } from "@mui/material";
+
+import { ProductsCart } from "@/components/cart/products-cart/products-cart";
+import { SectionWrapper } from "@/features/components/section-wrapper/section-wrapper";
+import { ShowMoreButton } from "@/features/components/show-more-button/show-more-button";
+import { ButtonMoveToCheckout } from "@/components/cart/button-move-to-checkout/button-move-to-checkout";
+
+export default function CartLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <>
+      <SectionWrapper>
+        <Typography component="h1" variant="h1">
+          Ваш кошик
+        </Typography>
+
+        <ShowMoreButton>Продовжити покупки</ShowMoreButton>
+      </SectionWrapper>
+
+      <SectionWrapper className="py-0">
+        <ProductsCart />
+      </SectionWrapper>
+
+      <ButtonMoveToCheckout />
+      {children}
+    </>
+  );
+}
