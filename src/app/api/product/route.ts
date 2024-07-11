98a8f67aebdcf2437dd8b7e8f457dd9c83ab2fef
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-11 14:12:04 +0300

    feat: update route to handle pagination

--- src/app/api/product/route.ts
+++ src/app/api/product/route.ts
@@ -41,10 +41,16 @@ export const GET = async (req: NextRequest): Promise<NextResponse> => {
   try {
     await connectToDB();
 
-    const products = await Product.find();
+    const url = new URL(req.url);
+    const page = parseInt(url.searchParams.get("page") || "1", 10);
+    const limit = parseInt(url.searchParams.get("limit") || "10", 10);
+    const skip = (page - 1) * limit;
+
+    const products = await Product.find().skip(skip).limit(limit);
 
     return NextResponse.json(products);
   } catch (error) {
+    console.error("Failed to get products:", error);
     return NextResponse.json(
       { error: "Failed to get products" },
       { status: 500 }
