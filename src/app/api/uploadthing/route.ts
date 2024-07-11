0710e16cb3c38b5469e15261a1efdab4121cb4ad
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-11 11:58:52 +0300

    feat: create route to delete file from uploadthing

--- src/app/api/uploadthing/route.ts
+++ src/app/api/uploadthing/route.ts
@@ -1,11 +1,22 @@
+import { NextRequest, NextResponse } from "next/server";
+
 import { createRouteHandler } from "uploadthing/next";
- 
+import { UTApi } from "uploadthing/server";
+
 import { ourFileRouter } from "./core";
- 
-// Export routes for Next App Router
+
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
- 
-  // Apply an (optional) custom config:
-  // config: { ... },
 });
+
+export async function DELETE(request: NextRequest): Promise<NextResponse> {
+  const data = await request.json();
+
+  const utapi = new UTApi();
+  await utapi.deleteFiles(data.key);
+
+  return NextResponse.json({
+    message: "File deleted successfully",
+    status: 200,
+  });
+}
