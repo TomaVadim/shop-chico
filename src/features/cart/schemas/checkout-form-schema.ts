9d17d9799c3f0b39918ff76a9b809198421298ff
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-11 11:54:03 +0300

    feat: create enums for cart selects

--- src/features/cart/schemas/checkout-form-schema.ts
+++ src/features/cart/schemas/checkout-form-schema.ts
@@ -1,15 +1,27 @@
+import { productSchema } from "@/features/products/schemas/product-data";
 import { z } from "zod";
 
+const PHONE_REGEX = /^[+]?[(]?[0-9]{3}[)]?[-\s.]?[0-9]{3}[-\s.]?[0-9]{4,6}$/im;
+
 export const checkoutFormSchema = z.object({
-  name: z.string().min(1, "Поле не повинно бути порожнім"),
-  surname: z.string().min(1, "Поле не повинно бути порожнім"),
-  phone: z
+  name: z
     .string()
     .min(1, "Поле не повинно бути порожнім")
-    .min(10, "Номер повинен містити 10 цифр"),
+    .refine((value) => !value.includes(" "), "Поле не повинно містити пробіли"),
+  surname: z
+    .string()
+    .min(1, "Поле не повинно бути порожнім")
+    .refine((value) => !value.includes(" "), "Поле не повинно містити пробіли"),
+  phone: z
+    .string()
+    .regex(PHONE_REGEX, "Введіть коректний номер телефону")
+    .min(1, "Поле не повинно бути порожнім")
+    .min(10, "Номер повинен містити не менше 10 цифр"),
   address: z.string().min(1, "Поле не повинно бути порожнім"),
   payment: z.string().min(1, "Поле не повинно бути порожнім"),
+  delivery: z.string().min(1, "Поле не повинно бути порожнім"),
   comment: z.string().optional(),
+  cart: z.array(productSchema).optional(),
 });
 
 export type CheckoutFormData = z.infer<typeof checkoutFormSchema>;
---
+++ src/features/cart/shared/enums/delivery-method.ts
@@ -0,0 +1,4 @@
+export const enum DELIVERY_METHOD {
+  NOVA_POSHTA = "Нова Пошта",
+  UKR_POSHTA = "УкрПошта",
+}
