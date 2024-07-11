fafc80004d1774712b6330e953b0948f1cad2da7
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-11 14:12:24 +0300

    feat: create pagination component

---
+++ src/components/products/pagination/pagination.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { Pagination as MuiPagination, Stack } from "@mui/material";
+
+interface Props {
+  count: number;
+  page: number;
+  onChange: (value: number) => void;
+}
+
+export const Pagination = ({ count, page, onChange }: Props): JSX.Element => {
+  return (
+    <Stack spacing={2}>
+      <MuiPagination
+        className="flex justify-center"
+        count={count}
+        page={page}
+        color="primary"
+        onChange={(event, value) => onChange(value)}
+      />
+    </Stack>
+  );
+};
