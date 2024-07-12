6019d80d4990415b4902ee64b482278f04a9826f
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-12 21:25:43 +0300

    chore: update admin navigation list

--- src/features/admin/constants/navigation-list.ts
+++ src/features/admin/constants/navigation-list.ts
@@ -3,7 +3,7 @@ import { PRIVATE_ROUTES } from "@/shared/enums/routes/private-routes";
 
 export const NAVIGATION_LIST: NavigationItem[] = [
   { id: 1, href: PRIVATE_ROUTES.CREATE, page: "+ Додати" },
-  { id: 2, href: "/admin", page: "Замовлення" },
+  { id: 2, href: PRIVATE_ROUTES.ORDERS, page: "Замовлення" },
   { id: 3, href: "/admin", page: "пусто" },
   { id: 4, href: "/admin", page: "пусто" },
   { id: 5, href: "/admin", page: "пусто" },
