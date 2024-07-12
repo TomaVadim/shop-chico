47fd7fe039df27a7b0423aa405c5b53bddbcb35f
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-12 21:21:47 +0300

    chore: change company logo

--- src/features/components/company-logo/company-logo.tsx
+++ src/features/components/company-logo/company-logo.tsx
@@ -1,8 +1,10 @@
 import Link from "next/link";
+import Image from "next/image";
 
 import { Typography } from "@mui/material";
 
 import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
+import babyImage from "@/assets/images/home/baby.png";
 
 export const CompanyLogo = (): JSX.Element => {
   return (
@@ -10,10 +12,11 @@ export const CompanyLogo = (): JSX.Element => {
       component={Link}
       variant="h3"
       rel="noreferrer nofollow"
-      className="text-black underline font-bold decoration-purple-500"
+      className="text-black flex items-center gap-1 underline font-bold decoration-purple-500"
       href={PUBLIC_ROUTES.HOME}
     >
-      @chehlidetkam
+      <Image src={babyImage} alt="chehlidetkam" width={25} height={25} />
+      <span className="mt-auto">chehlidetkam</span>
     </Typography>
   );
 };
