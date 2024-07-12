aabbda4c326b25b4571fc58783f465c80ff43365
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-12 16:04:19 +0300

    feat: add functionality to check is product existing on db

--- src/components/cart/products-cart/products-cart.tsx
+++ src/components/cart/products-cart/products-cart.tsx
@@ -1,16 +1,34 @@
 "use client";
 import { Box, Paper, Typography } from "@mui/material";
-
 import { CartCard } from "@/components/cart/cart-card/cart-card";
 import {
   useCartStore,
   useInitializeCart,
 } from "@/stores/zustand/use-cart-store";
+import { useEffect } from "react";
+import { fetchProducts } from "@/api/fetch-products";
+import { useCartQuantity } from "@/stores/zustand/use-cart-quantity";
 
 export const ProductsCart = (): JSX.Element => {
   useInitializeCart();
 
-  const { items: products } = useCartStore();
+  const { items: products, removeItem } = useCartStore();
+  const { decrement } = useCartQuantity();
+
+  useEffect(() => {
+    fetchProducts().then((data) => {
+      const currentProductIds = new Set(data.map((product) => product.id));
+
+      const productsToRemove = products.filter(
+        (product) => !currentProductIds.has(product.id)
+      );
+
+      productsToRemove.forEach((product) => {
+        removeItem(product.id);
+        decrement();
+      });
+    });
+  }, [products, removeItem, decrement]);
 
   const fullPrice = products.reduce(
     (acc, product) => acc + product.price * product.quantity,
