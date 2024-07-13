42d2e2a0c8c7d305b46baab06a740cf1d39bd4aa
TomaVadim <109627875+TomaVadim@users.noreply.github.com>
2024-07-13 16:01:29 +0300

    chore: change path by default redirect path

--- src/features/components/floating-action-button/floating-action-button.tsx
+++ src/features/components/floating-action-button/floating-action-button.tsx
@@ -1,14 +1,16 @@
+import Link from "next/link";
+
 import { Fab } from "@mui/material";
 import NotificationsActiveOutlinedIcon from "@mui/icons-material/NotificationsActiveOutlined";
-import Link from "next/link";
-import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
+
+import { DEFAULT_REDIRECT_PATH_TO_PRODUCTS } from "@/constants/default-redirect-path-to-products";
 
 export const FloatingActionButton = (): JSX.Element => {
   return (
     <Fab
       LinkComponent={Link}
       size="medium"
-      href={PUBLIC_ROUTES.PRODUCTS}
+      href={DEFAULT_REDIRECT_PATH_TO_PRODUCTS}
       color="secondary"
       aria-label="move-to-products-page"
       className="fixed bottom-5 left-5 animate-bounce opacity-95"
--- src/features/components/show-more-button/show-more-button.tsx
+++ src/features/components/show-more-button/show-more-button.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { Button } from "@mui/material";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 
-import { PUBLIC_ROUTES } from "@/shared/enums/routes/public-routes";
+import { DEFAULT_REDIRECT_PATH_TO_PRODUCTS } from "@/constants/default-redirect-path-to-products";
 
 export const ShowMoreButton = ({ children }: { children: string }) => {
   return (
@@ -13,7 +13,7 @@ export const ShowMoreButton = ({ children }: { children: string }) => {
       variant="contained"
       className="mt-10 mx-auto"
       component={Link}
-      href={`${PUBLIC_ROUTES.PRODUCTS}?gender=all&insert=without`}
+      href={DEFAULT_REDIRECT_PATH_TO_PRODUCTS}
     >
       {children}
     </Button>
